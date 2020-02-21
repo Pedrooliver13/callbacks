87de842692a8fa7fb7cb8d3751f74ce86380fa8e
Pedro Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-21 16:35:23 -0300

    Create callbacks.js

---
+++ callbacks.js
@@ -0,0 +1,69 @@
+//PRIMEIRA FORMA
+
+// MODELS
+
+all(callback){
+  db.query(`
+    SELECT * FROM callbacks
+  ` , (err, results)=>{
+    if(err) throw `Database is ${err}`
+
+    callback(results.rows)
+  })
+}
+
+//CONTROLLERS
+const models = require('...')
+
+module.esports = {
+  create(req, res){
+    models.all(function(results){
+      return res.render(''project/create' , { results })
+    }
+  }
+}
+
+//SEGUNDA FORMA USANDO PROMESSAS
+
+//models
+all(){
+  db.query(`
+  SELECT * FROM callbacks
+  `)
+}
+
+//CONTROLLERS
+const models = require('...')
+
+create(req, res){
+  models.all()
+  .then(function(results){
+    const project = results.rows
+    
+    return res.render('project/create' , { project })
+  }).cath(function(err){
+    throw new Error(err)
+  })
+}
+
+//TERCEIRA E MELHOR FORMA
+//USANDO ASYNC-AWAIT
+
+//models
+all(){
+  db.query(`
+    SELECT * FROM callbacks
+  `)
+}
+
+//controllers
+const models = require('...')
+
+module.exports ={
+  async create(req, res){
+    let results = await models.all()
+    const products = results.rows
+    
+    return res.render('products/create' , { products })
+  }
+}
